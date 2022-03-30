0e8c87a9776ecdd08dee7acac5d253566d2a0f1e
amanzanom7 <amanzanom7@hotmail.com>
2022-03-30 16:58:19 -0600

    TheMovieDB
    TheMovieDB

---
+++ TheMovieDB/TheMovieDB/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by MANZANO MARTINEZ, ALAIN MARIANO on 30/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+		
+		self.view.backgroundColor = .red
+	}
+
+
+}
+
