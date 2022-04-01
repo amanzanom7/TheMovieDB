d0ac59e6c59ec9c6f1fa14f6b92edb7c74f0b47b
amanzanom7 <amanzanom7@hotmail.com>
2022-04-01 17:16:09 -0600

    space

--- TheMovieDB/TheMovieDB/SceneDelegate.swift
+++ TheMovieDB/TheMovieDB/SceneDelegate.swift
@@ -20,7 +20,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		
 		window = UIWindow(windowScene: windowScene)
 		let viewController = ViewController()
-
 		window?.rootViewController = viewController
 		window?.makeKeyAndVisible()
 	}
