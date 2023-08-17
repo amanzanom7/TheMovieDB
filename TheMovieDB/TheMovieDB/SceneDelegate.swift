6148b142a902e96b27e67796df5775eb572d5b1c
Alain Mariano Manzano Martinez <amanzano@ia.com.mx>
2023-08-17 11:23:43 -0600

    delete unnecesary

--- TheMovieDB/TheMovieDB/SceneDelegate.swift
+++ TheMovieDB/TheMovieDB/SceneDelegate.swift
@@ -19,7 +19,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		guard let windowScene = (scene as? UIWindowScene) else { return }
 		
 		window = UIWindow(windowScene: windowScene)
-		print("Application directory: \(NSHomeDirectory())")
 		let viewController = ViewController()
 
 		window?.rootViewController = viewController
